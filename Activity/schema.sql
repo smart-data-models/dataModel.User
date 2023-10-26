4bd670c650679e183100bfd03ed160bb06e0e273
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:50:09 +0200

    test of SQL export

--- Activity/schema.sql
+++ Activity/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Activity of the subject dataModel.User for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Activity_type AS ENUM ('Activity');
-CREATE TABLE Activity (activityType TEXT, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateActivityEnded TIMESTAMP, dateActivityStarted TIMESTAMP, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Activity_type);
+CREATE TABLE Activity (activityType TEXT, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateActivityEnded TIMESTAMP, dateActivityStarted TIMESTAMP, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Activity_type);
