77bcb7e755c5fcd25f7c66237367277ce04623bf
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:15:23 +0200

    created example of code

--- Activity/code/code_for_using_dataModel.User_Activity.py
+++ Activity/code/code_for_using_dataModel.User_Activity.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Activity"
 subject = "dataModel.User"
-activityType = "{'type': 'Property', 'value': 'Drive'}"
+activityType = "Drive"
 attribute = "activityType"
 value = activityType
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateActivityStarted = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2016-11-30T07:00:00.00Z'}}"
+dateActivityStarted = "2016-11-30T07:00:00.00Z"
 attribute = "dateActivityStarted"
 value = dateActivityStarted
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refAgent = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Agent:User1'}"
+refAgent = "urn:ngsi-ld:Agent:User1"
 attribute = "refAgent"
 value = refAgent
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refObject = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Object:Car1'}"
+refObject = "urn:ngsi-ld:Object:Car1"
 attribute = "refObject"
 value = refObject
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
